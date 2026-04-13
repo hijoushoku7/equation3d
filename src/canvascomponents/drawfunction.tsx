a07b350d24e897a5e141d23c36c5266becca04c4
Hijoushoku <hijoushoku88@gmail.com>
2026-04-13 23:17:17 +0900

    third commit

--- src/canvascomponents/drawfunction.tsx
+++ src/canvascomponents/drawfunction.tsx
@@ -1,20 +1,59 @@
 import './drawfunction.css'
 
 function DrawFunction() {
-  window.addEventListener('load', draw);
+  window.addEventListener('load', setup);
 
-  function draw() {
+  const canvasSize = {
+    x: 8000,
+    y: 8000,
+  }
+  const originX = canvasSize.x / 2;
+  const originY = canvasSize.y / 2;
+
+
+  const calcY = (x) => {
+    return x * 2;
+  }
+  let x = 0;
+  const dx = 1;
+  const max = canvasSize.x;
+
+  async function setup() {
+    await drawOriginLine();
+    await drawFunction(x, max, dx)
+  }
+
+  function drawOriginLine() {
     const canvas = document.getElementById('draw-canvas') as HTMLCanvasElement;
     const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+    if (!canvas || !ctx) return;
     ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(1500, 1500);
+    ctx.moveTo(originX, canvasSize.y);
+    ctx.lineTo(originX, 0);
+    ctx.moveTo(0, originY);
+    ctx.lineTo(canvasSize.x, originY);
     ctx.stroke();
   }
 
+
+  function drawFunction(x, maxX, dx) {
+    const canvas = document.getElementById('draw-canvas') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+    ctx.moveTo(0, 0);
+    for (let X = x; X <= max; X += dx) {
+      const y = calcY(X) - canvasSize.y / 2;
+      if (!(y > max)) { ctx.lineTo(X, y); }
+      ctx.moveTo(X, y);
+      // console.log(`xは${x} 、yは${y}`);
+    }
+    ctx.stroke();
+  }
+
+
   return (
     <>
-      <canvas id='draw-canvas' width={1500} height={1500}></canvas>
+      <canvas id='draw-canvas'></canvas>
     </>
   )
 }
