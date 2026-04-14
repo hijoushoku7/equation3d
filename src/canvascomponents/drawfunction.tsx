53c5b49f50f8a653a59f345e6d48a96424f49a1b
Hijoushoku <hijoushoku88@gmail.com>
2026-04-14 09:44:52 +0900

    added drawfunction

--- src/canvascomponents/drawfunction.tsx
+++ src/canvascomponents/drawfunction.tsx
@@ -10,25 +10,38 @@ function DrawFunction() {
     y: 1000,
   }
   const functionNum = {
-    x: (t) => { return Math.cos(t) - t },
-    y: (t) => { return 1 - Math.cos(t) },
-    FirstT: 0,
-    MaxT: 200,
+    x: (t) => { return 10 * (Math.sin(t) - t) },
+    y: (t) => { return 10 * (1 - Math.cos(t)) },
+    FirstT: -2000,
+    MaxT: 2000,
     dt: 0.1,
+  }
+  const camera = {
+    x: 0, y: 0, z: 0,
+    zx: 0, zy: 0,
   }
 
   async function setup() {
     const canvas = document.getElementById('draw-canvas') as HTMLCanvasElement;
     const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+    drawFunction2d(functionNum, ctx);
   }
   function drawFunction2d(functionNum, ctx) {
     ctx.beginPath();
+    if (!ctx) return;
+    for (let t = functionNum.FirstT; t <= functionNum.MaxT; t += functionNum.dt) {
+      const x = canvasSize.x / 2 + functionNum.x(t);
+      const y = canvasSize.y / 2 - functionNum.y(t);
+      if (x < canvasSize.x && y < canvasSize.y) ctx.lineTo(x, y);
+      ctx.moveTo(x, y);
+    }
 
+    ctx.stroke();
   }
 
   return (
     <>
-      <canvas id='draw-canvas'></canvas>
+      <canvas id='draw-canvas' width={canvasSize.x} height={canvasSize.y}></canvas>
     </>
   )
 }
